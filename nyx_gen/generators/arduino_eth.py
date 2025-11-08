d2318a9e349527f60baaf1fba171e09b45e8b9d5
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2025-11-08 23:12:42 +0100

    update

--- nyx_gen/generators/arduino_eth.py
+++ nyx_gen/generators/arduino_eth.py
@@ -5,17 +5,58 @@ import os
 
 ########################################################################################################################
 
-from ..abstract_generator import AbstractGenerator, generator_config
+from .posix_c import PosixCGenerator
+
+from ..abstract_generator import generator_config
 
 ########################################################################################################################
 
 @generator_config(name = 'arduino-eth', null = 'nullptr', src_ext = 'cpp', head_ext = 'hpp')
-class ArduinoEthGenerator(AbstractGenerator):
+class ArduinoEthGenerator(PosixCGenerator):
 
     ####################################################################################################################
 
-    def generate(self) -> None:
+    def _generate_cmake(self) -> None:
 
-        pass
+        platform, board, ram = self._descr['board'].split('|')
+
+        template = f'''
+; PlatformIO Project Configuration File
+;
+;   Build options: build flags, source filter
+;   Upload options: custom upload port, speed and extra flags
+;   Library options: dependencies, extra library storages
+;   Advanced options: extra scripting
+;
+; Please visit documentation for the other options and examples
+; https://docs.platformio.org/page/projectconf.html
+
+[env]
+build_flags = -DNYX_HAS_ETHERNET -DNYX_RAM_SIZE={ram} -Wno-unused-function
+
+lib_deps = PubSubClient, arduino-libraries/Ethernet, https://github.com/nyxlib/nyx-node.git
+
+lib_ignore = WiFiS3, FATFilesystem
+
+monitor_speed = 115200
+
+[env:nyx_node]
+platform = {platform}
+board = {board}
+framework = arduino
+'''[1:]
+
+        filename = os.path.join(self._driver_path, 'platformio.ini')
+
+        if self._override_cmake or not os.path.isfile(filename):
+
+            with open(filename, 'wt', encoding = 'utf-8') as f:
+
+                f.write(self.render(
+                    template,
+                    platform = platform,
+                    board = board,
+                    ram = ram
+                ))
 
 ########################################################################################################################
